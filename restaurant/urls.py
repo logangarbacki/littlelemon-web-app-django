33f6f3b3154b2d2cf51427264a87b721c7bcbf08
Logan Garbacki <lgarbacki@printscan.com>
2026-04-10 17:29:19 -0400

    Wire up API URLs and Djoser authentication endpoints

---
+++ restaurant/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+from . import views
+
+router = DefaultRouter()
+router.register(r'bookings', views.BookingViewSet, basename='booking')
+
+urlpatterns = [
+    path('menu/', views.MenuItemView.as_view(), name='menu'),
+    path('menu/<int:pk>/', views.SingleMenuItemView.as_view(), name='menu-item'),
+] + router.urls
