d993a5310166c92b9fb288aad76631bdff43a21a
Logan Garbacki <lgarbacki@printscan.com>
2026-04-10 16:55:07 -0400

    Add initial migration and connect to MySQL database

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'littlelemon',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'admin123',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
