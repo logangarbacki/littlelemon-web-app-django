c30aab12265c13d3a7ccdc734fa002b244687fd6
Logan Garbacki <lgarbacki@printscan.com>
2026-04-10 17:26:49 -0400

    Add MenuItem and Booking API views

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import MenuItem, Booking
+from .serializers import MenuItemSerializer, BookingSerializer
 
-# Create your views here.
+
+class MenuItemView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class SingleMenuItemView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    permission_classes = [IsAuthenticated]
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = [IsAuthenticated]
