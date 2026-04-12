2e546ddea2280efead3df05dcf67dd301a082d38
Logan Garbacki <lgarbacki@printscan.com>
2026-04-12 02:44:04 -0400

    Remove SessionAuthentication to fix CSRF error on API token login

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -142,7 +142,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
